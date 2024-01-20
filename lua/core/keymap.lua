3c77314d106b3fd5a6a8fccbd6ab19854f36b485
Josef Utbult <josef@utbult.design>
2024-01-20 11:16:28 +0100

    Init

---
+++ lua/core/keymap.lua
@@ -0,0 +1,12 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = false
+vim.opt.autoread = true
+
+-- Clear the search on <leader>h
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
---
+++ lua/core/plugins.lua
@@ -0,0 +1,22 @@
+-- Check if packer is installed. Install it otherwise
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  -- Plugins
+  use 'wbthomason/packer.nvim'
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
