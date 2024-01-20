6468f7e26332b5f5e43d85243ad88f2c4606831d
Josef Utbult <josef@utbult.design>
2024-01-20 13:56:42 +0100

    Fix transperant background

--- lua/core/plugin_config/gruvbox.lua
+++ lua/core/plugin_config/gruvbox.lua
@@ -1,4 +1,7 @@
 -- Gruvbox theme
 -- If changed, remember to change in LuaLine
-vim.o.termguicolors = true
+
+require("gruvbox").setup({
+	transparent_mode = true
+})
 vim.cmd [[ colorscheme gruvbox ]]
