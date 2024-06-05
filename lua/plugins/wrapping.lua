fa3f533c56d28af18bcf7d490ca4a71113a04726
Josef Utbult <josef@clavia.se>
2024-06-05 15:30:07 +0200

    Fix wrapping

--- lua/plugins/wrapping.lua
+++ lua/plugins/wrapping.lua
@@ -1,9 +1,15 @@
--- Wrapping modes:
---	[ow - soft wrap mode
---	]ow - hard wrap mode
---	yow - toggle wrap mode
 return {
 	{
-		"andrewferrier/wrapping.nvim"
+		"andrewferrier/wrapping.nvim",
+		keys = {
+			{
+				"<leader>aw",
+				"<CMD>ToggleWrapMode<CR>",
+				desc = "Toggle WrapMode"
+			}
+		},
+		config = function()
+			require("wrapping").setup()
+		end
 	},
 }
